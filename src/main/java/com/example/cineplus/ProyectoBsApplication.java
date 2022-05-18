98b271321cbe313490e26755650b89a829babf82
UBF21 <felipe_2_0_0_2@hotmail.com>
2022-05-18 03:01:21 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/example/cineplus/ProyectoBsApplication.java
@@ -0,0 +1,13 @@
+package com.example.cineplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cineplus/ProyectoBsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cineplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
